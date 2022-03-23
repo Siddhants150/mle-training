b46f71c51e7c63cb0002d4858bc0d5ee8680e81b
siddhant sharma <siddhants150@tigeranalytics.com>
2022-03-24 01:00:40 +0530

    logging added

--- src/ingestData.py
+++ src/ingestData.py
@@ -8,14 +8,23 @@ import logging
 from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 from six.moves import urllib
 from sklearn.impute import SimpleImputer
+from datetime import date
+
+today = date.today()
+
+d1 = today.strftime("%d/%m/%Y")
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 saveTrainTestPath = os.path.join('datasets', 'data')
+logPath = os.path.join('logs', d1)
+logLevel = logging.INFO
+consoleLog = 1
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    logger.debug("Inside the fetch housing data function")
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
@@ -24,13 +33,16 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.close()
 
 def load_housing_data(housing_path=HOUSING_PATH):
+    logger.debug("Inside the load house data function")
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
 def income_cat_proportions(data):
+    logger.debug("Inside the income cat function")
     return data["income_cat"].value_counts() / len(data)
 
 def data_prepared(data, fileName = 'train'):
+    logger.debug("Inside the data prepared function")
     dataX = data.drop('median_house_value', axis = 1)
     dataY = data['median_house_value'].copy()
     dataNum = dataX.drop('ocean_proximity', axis = 1)
@@ -44,30 +56,78 @@ def data_prepared(data, fileName = 'train'):
     dataTr["population_per_household"] = dataTr["population"] / dataTr["households"]
     dataPrepared = dataTr.join(pd.get_dummies(oceanProximityColumn, drop_first=True))
     dataPrepared['median_house_value'] = dataY
-    print("data from " + fileName)
-    print(dataPrepared.head())
+    logger.debug("data from " + fileName)
+    logger.info(dataPrepared.head())
     try:
         if not os.path.isdir(saveTrainTestPath):
             os.makedirs(saveTrainTestPath)
         dataPrepared.to_csv(os.path.join(saveTrainTestPath, fileName + '.csv'))
     except Exception as e:
-        print('Error occured')
-        print(e)
-    dataPrepared
+        logger.error('Error occured')
+        logger.error(e)
     
 try:
     argumentParser = argparse.ArgumentParser(prog = 'ingestData', description = 'Ingesting Data')
     argumentParser.add_argument('--folder', action = 'store', type = str, required = False)
     argumentParser.add_argument('--trainfile', action = 'store', type = str, required = False)
     argumentParser.add_argument('--testfile', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--log_level', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--log_path', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--no_console_log', action = 'store_true',  required = False)
     arguments = argumentParser.parse_args()
     if arguments.folder:
         saveTrainTestPath = os.path.join('datasets', arguments.folder)
+    if arguments.log_path:
+        logFolder = os.path.join(arguments.log_path, 'ingestData.txt')
+
     print(arguments)
 
 except Exception as e:
     print("Error Occured")
     print(e)
+
+try:
+    if arguments.log_level == "DEBUG":
+        logLevel = logging.DEBUG
+    elif arguments.log_level == "INFO":
+        logLevel = logging.INFO
+    elif arguments.log_level == "WARNING":
+        logLevel = logging.WARNING
+    elif arguments.log_level == "ERROR":
+        logLevel = logging.ERROR
+    elif arguments.log_level == "CRITICAL":
+        logLevel = logging.CRITICAL
+
+    if arguments.log_path:
+        logPath = os.path.join(arguments.log_path, "logs")
+
+    if not os.path.isdir(logPath):
+            os.makedirs(logPath)
+    
+    logFile = os.path.join(logPath, "ingestData.log")
+
+    if arguments.no_console_log:
+        consoleLog = 0
+
+    try:
+        logging.basicConfig(filename = logFile, level = logLevel)
+        logger = logging.getLogger("ingestData")
+    except Exception as e:
+        print("error occured")
+        print(e)
+    
+    if consoleLog:
+        ch = logging.StreamHandler()
+        ch.setLevel(logLevel)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s -%(levelname)s - %(message)s"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+except Exception as e:
+    logger.error("Error Occured")
+    logger.error(e)
 
 
 try:
@@ -83,8 +143,10 @@ try:
     for train_index, test_index in split.split(housing, housing["income_cat"]):
         strat_train_set = housing.loc[train_index]
         strat_test_set = housing.loc[test_index]
-    
+    logger.debug("Strat Train and Test Set Generated")
+
     train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+    logger.debug('Train and Test Set Generated')
 
     compare_props = pd.DataFrame(
         {
@@ -99,6 +161,8 @@ try:
     compare_props["Strat. %error"] = (
         100 * compare_props["Stratified"] / compare_props["Overall"] - 100
     )
+    logger.info('compare props')
+    logger.info(compare_props)
 
     for set_ in (strat_train_set, strat_test_set):
         set_.drop("income_cat", axis=1, inplace=True)
@@ -116,5 +180,5 @@ try:
     # data_prepared(strat_test_set, "test")
 
 except Exception as e:
-    print("Error Occured")
-    print(e)
+    logger.error("Error Occured")
+    logger.error(e)
--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pickle
 import os
 import argparse
+import logging
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
@@ -9,29 +10,44 @@ from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import randint
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from datetime import date
+
+today = date.today()
+
+d1 = today.strftime("%d/%m/%Y")
 
 modelFolder = os.path.join('artifacts', 'models')
 inputFolder = os.path.join('datasets', 'data')
 
+logPath = os.path.join('logs', d1)
+logLevel = logging.INFO
+consoleLog = 1
+
 def trainLinearModel(X, Y, fileName):
+    logger.debug('Entered train linear regression function')
     linearRegression = LinearRegression()
     linearRegression.fit(X, Y)
     YPred = linearRegression.predict(X)
     mse = mean_squared_error(Y, YPred)
     rmse = np.sqrt(mse)
-    print('Linear Regression RMSE on Training: ', rmse)
+    logger.info('Linear Regression RMSE on Training: ')
+    logger.info(rmse)
     pickle.dump(linearRegression, open(os.path.join(modelFolder, fileName), 'wb'))
+    
 
 def trainTreeRegressor(X, Y, fileName):
+    logger.debug('Entered train tree regression function')
     DTR = DecisionTreeRegressor(random_state = 42)
     DTR.fit(X, Y)
     YPred = DTR.predict(X)
     mse = mean_squared_error(Y, YPred)
     rmse = np.sqrt(mse)
-    print('Decision Tree Regressor Regression RMSE on Training: ', rmse)
+    logger.info('Decision Tree Regressor Regression RMSE on Training: ')
+    logger.info(rmse)
     pickle.dump(DTR, open(os.path.join(modelFolder, fileName), 'wb'))
 
 def trainRandomForestRegressorRandomSearch(X, Y, fileName):
+    logger.debug('Entered train random regression with random search function')
     RFR = RandomForestRegressor()
     param_distribs = {
         "n_estimators": randint(low=1, high=200),
@@ -50,10 +66,12 @@ def trainRandomForestRegressorRandomSearch(X, Y, fileName):
     YPred = model.predict(X)
     mse = mean_squared_error(Y, YPred)
     rmse = np.sqrt(mse)
-    print('Random Forest Randomized Search RMSE on Training: ', rmse)
+    logger.info('Random Forest Randomized Search RMSE on Training: ')
+    logger.info(rmse)
     pickle.dump(model, open(os.path.join(modelFolder, fileName), 'wb'))
 
 def trainRandomForestRegressorGridSearch(X, Y, fileName):
+    logger.debug('Entered train random forest regression with grid search function')
     param_grid = [
         {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
         {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
@@ -71,7 +89,8 @@ def trainRandomForestRegressorGridSearch(X, Y, fileName):
     YPred = model.predict(X)
     mse = mean_squared_error(Y, YPred)
     rmse = np.sqrt(mse)
-    print('Random Forest Regressor Grid Search RMSE on Training: ', rmse)
+    logger.info('Random Forest Regressor Grid Search RMSE on Training: ')
+    logger.info(rmse)
     pickle.dump(model, open(os.path.join(modelFolder, fileName), 'wb'))
 
 try:
@@ -79,16 +98,64 @@ try:
     argumentParser.add_argument('--trainfolder', action = 'store', type = str, required = False)
     argumentParser.add_argument('--trainfile', action = 'store', type = str, required = False)
     argumentParser.add_argument('--outputfolder', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--log_level', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--log_path', action = 'store', type = str, required = False)
+    argumentParser.add_argument('--no_console_log', action = 'store_true',  required = False)
     arguments = argumentParser.parse_args()
     if arguments.trainfolder:
         inputFolder = os.path.join('datasets', arguments.trainfolder)
     if arguments.outputfolder:
         modelFolder = os.path.join('artifacts', arguments.outputfolder)
+    if arguments.log_path:
+        logFolder = os.path.join(arguments.log_path, 'ingestData.txt')
     print(arguments)
 
 except Exception as e:
     print('error occured')
     print(e)
+
+try:
+    if arguments.log_level == "DEBUG":
+        logLevel = logging.DEBUG
+    elif arguments.log_level == "INFO":
+        logLevel = logging.INFO
+    elif arguments.log_level == "WARNING":
+        logLevel = logging.WARNING
+    elif arguments.log_level == "ERROR":
+        logLevel = logging.ERROR
+    elif arguments.log_level == "CRITICAL":
+        logLevel = logging.CRITICAL
+
+    if arguments.log_path:
+        logPath = os.path.join(arguments.log_path, "logs")
+
+    if not os.path.isdir(logPath):
+            os.makedirs(logPath)
+    
+    logFile = os.path.join(logPath, "train.log")
+
+    if arguments.no_console_log:
+        consoleLog = 0
+
+    try:
+        logging.basicConfig(filename = logFile, level = logLevel)
+        logger = logging.getLogger("train")
+    except Exception as e:
+        print("error occured")
+        print(e)
+    
+    if consoleLog:
+        ch = logging.StreamHandler()
+        ch.setLevel(logLevel)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s -%(levelname)s - %(message)s"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+except Exception as e:
+    logger.error("Error Occured")
+    logger.error(e)
 
 try:
     if arguments.trainfile:
@@ -108,6 +175,6 @@ try:
     trainRandomForestRegressorGridSearch(trainX, trainY, 'randomForestRegressorWithGridSearch')
 
 except Exception as e:
-    print('error occured')
-    print(e)
+    logger.error('error occured')
+    logger.error(e)
     
